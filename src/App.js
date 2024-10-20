9c2927931263ef83c63a88bbd49a4088c871d752
6303217506 <vanamdhanushnetha@gmail.com>
2024-10-20 11:56:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './components/Home';
+import ThankYouPage from './components/ThankYouPage';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/thankyou" element={<ThankYouPage/>}/>
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
